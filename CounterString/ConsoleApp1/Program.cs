a948ad3d245f1e6c0e1df5ce62227e68df356c56
Dattobias <152405844+datto2530@users.noreply.github.com>
2024-01-03 16:02:50 +0700

    da xong

---
+++ CounterString/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] s = { "c","h", "a","o" };
+            string inputS = Console.ReadLine();
+            int count = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i]==inputS) ;
+                {
+                    count+=1;
+                }
+            }
+            Console.WriteLine("So ki tu co trong chuoi la " + count);
+        }
+    }
+}
